5dadfdafa69c486cfc60c41ef686e547320f8138
KIM SEUNGWOO <77507261+bsstory@users.noreply.github.com>
2023-11-08 18:59:38 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-const process = require("process");
+const express = require('express');
+const app = express();
+const process = require('process');
 const { authorize } = require("./modules/auth");
 const { listEvents } = require("./modules/listEvents");
 
-/*
-function _addDays(date, days) {
-  var result = new Date(date);
-  result.setDate(result.getDate() + days);
-  return result;
-}
-*/
+app.get('/events', async (req, res) => {
+  try {
+    const auth = await authorize();
+    const cid = 'seungwoo@dcamp.kr'; // 또는 req.query.cid 사용
+    let day = '2023-11-09'; // 또는 req.query.day 사용
+    const results = await listEvents(auth, cid, day);
+    res.json(results); // 결과를 JSON 형식으로 반환
+  } catch (error) {
+    res.status(500).send('Internal Server Error');
+  }
+});
 
-(async () => {
-  const auth = await authorize();
-  const cid = 'seungwoo@dcamp.kr'; //process.argv[2];
-  let day = '2023-11-09'; // process.argv[3];
-  //const end = process.argv[4];
-
-  console.log(`Result ${cid} : ${day}`);
-  const Result = await listEvents(auth, cid, day);
-  console.log(Result);
-
-
-})();
+const PORT = process.env.PORT || 3000; // PORT 환경 변수 또는 기본값 3000 사용
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
