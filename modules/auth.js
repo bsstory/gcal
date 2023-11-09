82388d6b128dd66235ed205dc39071785db3bee6
KIM SEUNGWOO <77507261+bsstory@users.noreply.github.com>
2023-11-09 16:55:04 +0900

    Update auth.js

--- modules/auth.js
+++ modules/auth.js
@@ -1,8 +1,11 @@
-const fs = require('fs').promises;
-const { google } = require('googleapis');
-const path = require('path');
-const process = require('process');
+// From official quickstart at https://developers.google.com/calendar/api/quickstart/nodejs
+
+const fs = require("fs").promises;
+const path = require("path");
+const process = require("process");
+//const { authenticate } = require("@google-cloud/local-auth");
 const { OAuth2Client } = require('google-auth-library');
+const { google } = require("googleapis");
 
 const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
 const TOKEN_PATH = path.join(process.cwd(), 'credentials', 'token.json');
@@ -21,24 +24,31 @@ function authorize(credentials) {
   return oAuth2Client;
 }
 
-async function checkToken(oAuth2Client) {
+async function checkToken(oAuth2Client, res) {
   // Check if we have previously stored a token.
   try {
     const token = await fs.readFile(TOKEN_PATH);
     oAuth2Client.setCredentials(JSON.parse(token));
   } catch (err) {
-    return getNewToken(oAuth2Client); // getNewToken will handle the user authorization flow
+    return getNewToken(oAuth2Client, res); // getNewToken 호출에 res 인자 추가
   }
+  
 }
 
-function getNewToken(oAuth2Client) {
+function getNewToken(oAuth2Client, res) {
   // Generate a url that asks permissions for the SCOPES
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: SCOPES,
   });
-
-  // Your code to redirect user to the authUrl
+// Redirect user to the authUrl or return it as a response
+  if (res) {
+    console.log(`리다이렉션중: ${authUrl}`);
+    res.redirect(authUrl); // Use 'res' for redirection
+  } else {
+    console.log(`Authorize this app by visiting this url: ${authUrl}`);
+    return authUrl; // Or return URL if 'res' is not provided
+  }
 }
 
 async function verifyToken(oAuth2Client, code) {
@@ -49,10 +59,10 @@ async function verifyToken(oAuth2Client, code) {
   await fs.writeFile(TOKEN_PATH, JSON.stringify(tokens));
 }
 
-async function getAuthenticatedClient() {
+async function getAuthenticatedClient(res) {
   const credentials = await loadCredentialsFile();
   const oAuth2Client = authorize(credentials);
-  await checkToken(oAuth2Client);
+  await checkToken(oAuth2Client, res);
   return oAuth2Client;
 }
 
