19286e9f8a0b5dd33d086ecf9b78fcd1f9251e6d
KIM SEUNGWOO <77507261+bsstory@users.noreply.github.com>
2023-11-09 16:37:11 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,24 @@ const { getAuthenticatedClient, verifyToken } = require('./modules/auth');
 const { listEvents } = require('./modules/listEvents');
 
 // 이 엔드포인트는 인증된 사용자의 이벤트를 나열합니다.
-app.get('/events', async (req, res) => {
-  try {
-    const auth = await getAuthenticatedClient();
-    const cid = 'seungwoo@dcamp.kr'; // 또는 req.query.cid 사용
-    let day = '2023-11-09'; // 또는 req.query.day 사용
-    const results = await listEvents(auth, cid, day);
-    res.json(results); // 결과를 JSON 형식으로 반환
-  } catch (error) {
-    console.error(error); // 오류를 콘솔에 기록
-    res.status(500).send('Internal Server Error');
+app.post('/chat', async function(req, res, next) {
+  const messageText = req.body.message.text;
+  if (messageText.startsWith('/캘린더조회 ')) {
+    const calendarName = messageText.replace('/캘린더조회 ', '').trim();
+    try {
+      const auth = await getAuthenticatedClient();
+      const cid = 'seungwoo@dcamp.kr'; // 또는 req.query.cid 사용
+      let day = '2023-11-09'; // 또는 req.query.day 사용
+      const results = await listEvents(auth, cid, day);
+      res.json(results); // 결과를 JSON 형식으로 반환
+    } catch (error) {
+      console.error(error); // 오류를 콘솔에 기록
+      res.status(500).send('Internal Server Error');
+    }
+  } else {
+    // 기본적으로 메시지를 그대로 반환
+    const reply = {text: messageText};
+    res.json(reply);
   }
 });
 
@@ -23,7 +31,7 @@ app.get('/callback', async (req, res) => {
   try {
     const code = req.query.code;
     await verifyToken(code); // 인증 코드를 검증하고 토큰을 저장합니다.
-    res.redirect('/events'); // 인증이 성공하면 /events 경로로 리디렉션합니다.
+    res.redirect('/chat'); // 인증이 성공하면 /events 경로로 리디렉션합니다.
   } catch (error) {
     console.error(error);
     res.status(500).send('Authentication failed');
